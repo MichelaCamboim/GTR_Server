0d6508a953fe81a33d74b19336bcdde69001620a
Michela Camboim <mcamboim@gmail.com>
2022-12-10 11:28:58 -0300

    server configured to handle photo and pdf

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import connect from "./config/db.config.js";
-import userRoute from "./routes/user.routes.js";
-import reportRoute from "./routes/report.routes.js";
-import taskRoute from "./routes/task.routes.js";
-import chatbotRoute from "./routes/chatbot.routes.js";
 import cors from "cors";
+import connect from "./config/db.config.js";
+
+import userRoute from "./routes/user.routes.js";
+import taskRoute from "./routes/task.routes.js";
+import reportRoute from "./routes/report.routes.js";
+import chatbotRoute from "./routes/chatbot.routes.js";
+
+import logRoute from "./routes/log.routes.js";
+import uploadRoute from "./routes/uploadFile.routes.js";
 
 dotenv.config();
-connect();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+connect();
 
 app.use("/user", userRoute);
 app.use("/task", taskRoute);
 app.use("/report", reportRoute);
 app.use("/chatbot", chatbotRoute);
+
+app.use("/log", logRoute);
+app.use("/fileUpload", uploadRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(
