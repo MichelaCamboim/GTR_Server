1fd0a28fac98f821e11916a2d732b9cce9715c75
stroligo <gabrielstroligo@gmail.com>
2022-12-10 00:27:33 +0000

    Log Update

---
+++ model/log.model.js
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+
+const logSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  task: { type: Schema.Types.ObjectId, ref: "Task" },
+  report: { type: Schema.Types.ObjectId, ref: "Report" },
+  date: { type: Date, default: Date.now },
+  status: { type: String },
+});
+
+const LogModel = model("Log", logSchema);
+
+export default LogModel;
