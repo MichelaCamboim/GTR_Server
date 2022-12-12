7bdcbce6077a7591606e1ba980382a62481e775b
nosderson <andersonrodriguesmoro@gmail.com>
2022-12-11 23:33:19 -0300

    corrigida parametro da rota

--- routes/mytasks.routes.js
+++ routes/mytasks.routes.js
@@ -8,20 +8,23 @@ const mytasksRoute = express.Router();
 
 mytasksRoute.get("/worked_hours/:init/:end", async (req, res) => {
 console.log(req.params.init)
-console.log("oi")
 console.log(req.params.end)
 });
 
-mytasksRoute.get("/status/type", async (req, res) => {
-  
+mytasksRoute.get("/outoftime", async (req, res) => {
+})
+
+mytasksRoute.get("/status/:type", async (req, res) => {
+  try {
+    const tasks = await TaskModel.find(
+      {status: req.params.type},
+      { __v: 0, createdAt: 0, updatedAt: 0 }
+    ).populate("members", "_id nome matricula");
+    return res.status(200).json(tasks);
+  } catch (error) {
+    return res.status(500).json(error.errors);
+  }
+    
 });
-
-
-mytasksRoute.get("/assigned", async (req, res) => {
-
-});
-
-
-
 
 export default mytasksRoute;
