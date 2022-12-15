b8c464538a564cc351cfefb5d099a9cc42d0735a
DemetriusJayme <115664765+DemetriusJayme@users.noreply.github.com>
2022-12-15 14:35:04 -0300

    quinta 5

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import ReportModel from "../model/report.model.js";
 import UserModel from "../model/user.model.js";
-
+​
 import isAuth from "../middleware/isAuth.js";
 import attachCurrentUser from "../middleware/attachCurrentUser.js";
 import isSuperv from "../middleware/isSuperv.js";
-
+import isDirector from "../middleware/isDirector.js"
+​
 const reportRoute = express.Router();
-
+​
 //CREATE REPORT
 reportRoute.post(
   "/create-report",
@@ -16,18 +17,20 @@ reportRoute.post(
   async (req, res) => {
     try {
       const newReport = await ReportModel.create({
-        ...req.body,
+        ...req.body, avaliador: req.currentUser._id
       });
-
+​
       //Inserir o usuario a ser avaliado
       const user = await UserModel.findByIdAndUpdate(
-        req.body.user,
+        req.body.avaliado,
         {
           $push: { report: newReport._id },
         },
         { new: true, runValidators: true }
       );
-
+​
+      
+​
       return res.status(201).json(newReport);
     } catch (error) {
       console.log(error);
@@ -35,19 +38,43 @@ reportRoute.post(
     }
   }
 );
-
+​
 //GET ALL
 reportRoute.get("/all", isAuth, attachCurrentUser, async (req, res) => {
   try {
-    const reports = await ReportModel.find().populate("user");
-
+    const reports = await ReportModel.find().populate("avaliado");
+​
     return res.status(200).json(reports);
   } catch (error) {
     console.log(error);
     return res.status(500).json(error.errors);
   }
 });
-
+​
+reportRoute.get("/avaliado/", isAuth, attachCurrentUser, async (req, res) => {
+  try {
+    const reports = await ReportModel.find({avaliado : req.currentUser._id});
+​
+    return res.status(200).json(reports);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+​
+reportRoute.get("/avaliador/", isAuth, attachCurrentUser, isSuperv, async (req, res) => {
+  try {
+    const reports = await ReportModel.find({avaliador : req.currentUser._id});
+​
+    return res.status(200).json(reports);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+​
+​
+​
 //GET ONE
 reportRoute.get(
   "/oneReport/:reportId",
@@ -59,14 +86,14 @@ reportRoute.get(
       const { reportId } = req.params;
       const report = await ReportModel.findById(reportId).populate("user");
       if (!report) return res.status(400).json({ msg: "Report not found!" });
-
+​
       return res.status(200).json(report);
     } catch (error) {
       return res.status(400).json(error.errors);
     }
   }
 );
-
+​
 //UPDATE
 reportRoute.put(
   "/edit/:reportId",
@@ -77,13 +104,13 @@ reportRoute.put(
     try {
       const { reportId } = req.params;
       console.log(reportId);
-
+​
       const updatedReport = await ReportModel.findByIdAndUpdate(
         { _id: reportId },
         { ...req.body },
         { new: true, runValidators: true }
       );
-
+​
       return res.status(200).json(updatedReport);
     } catch (error) {
       console.log(error);
@@ -91,9 +118,9 @@ reportRoute.put(
     }
   }
 );
-
+​
 //DELETE
-chatbotRoute.delete(
+reportRoute.delete(
   "/delete/:reportId",
   isAuth,
   attachCurrentUser,
@@ -101,24 +128,24 @@ chatbotRoute.delete(
   async (req, res) => {
     try {
       const { reportId } = req.params;
-
+​
       const deletedReport = await ReportModel.findByIdAndDelete(reportId);
-
+​
       if (!deletedReport) {
         return res.status(400).json({ msg: "Report not found!" });
       }
-
+​
       const allReports = await ReportModel.find();
       const user = await UserModel.findByIdAndUpdate(
-        deletedReport.user,
+        deletedReport.avaliado,
         {
           $pull: { report: deletedReport._id },
         },
         { new: true, runValidators: true }
       );
-
+​
       console.log(deletedReport);
-
+​
       return res.status(200).json(allReports);
     } catch (error) {
       console.log(error);
@@ -126,5 +153,5 @@ chatbotRoute.delete(
     }
   }
 );
-
+ 
 export default reportRoute;
