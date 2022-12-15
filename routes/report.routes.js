5c8008580ae1861c425529065ea1ba036fea4618
Michela Camboim <mcamboim@gmail.com>
2022-12-15 17:36:36 -0300

    rota report ajustada

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import ReportModel from "../model/report.model.js";
 import UserModel from "../model/user.model.js";
-​
+
 import isAuth from "../middleware/isAuth.js";
 import attachCurrentUser from "../middleware/attachCurrentUser.js";
 import isSuperv from "../middleware/isSuperv.js";
-import isDirector from "../middleware/isDirector.js"
-​
+import isDirector from "../middleware/isDirector.js";
+
 const reportRoute = express.Router();
-​
+
 //CREATE REPORT
 reportRoute.post(
   "/create-report",
@@ -17,9 +17,10 @@ reportRoute.post(
   async (req, res) => {
     try {
       const newReport = await ReportModel.create({
-        ...req.body, avaliador: req.currentUser._id
+        ...req.body,
+        avaliador: req.currentUser._id,
       });
-​
+
       //Inserir o usuario a ser avaliado
       const user = await UserModel.findByIdAndUpdate(
         req.body.avaliado,
@@ -28,9 +29,7 @@ reportRoute.post(
         },
         { new: true, runValidators: true }
       );
-​
-      
-​
+
       return res.status(201).json(newReport);
     } catch (error) {
       console.log(error);
@@ -38,43 +37,49 @@ reportRoute.post(
     }
   }
 );
-​
+
 //GET ALL
 reportRoute.get("/all", isAuth, attachCurrentUser, async (req, res) => {
   try {
     const reports = await ReportModel.find().populate("avaliado");
-​
+
     return res.status(200).json(reports);
   } catch (error) {
     console.log(error);
     return res.status(500).json(error.errors);
   }
 });
-​
+
 reportRoute.get("/avaliado/", isAuth, attachCurrentUser, async (req, res) => {
   try {
-    const reports = await ReportModel.find({avaliado : req.currentUser._id});
-​
+    const reports = await ReportModel.find({ avaliado: req.currentUser._id });
+
     return res.status(200).json(reports);
   } catch (error) {
     console.log(error);
     return res.status(500).json(error.errors);
   }
 });
-​
-reportRoute.get("/avaliador/", isAuth, attachCurrentUser, isSuperv, async (req, res) => {
-  try {
-    const reports = await ReportModel.find({avaliador : req.currentUser._id});
-​
-    return res.status(200).json(reports);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(error.errors);
+
+reportRoute.get(
+  "/avaliador/",
+  isAuth,
+  attachCurrentUser,
+  isSuperv,
+  async (req, res) => {
+    try {
+      const reports = await ReportModel.find({
+        avaliador: req.currentUser._id,
+      });
+
+      return res.status(200).json(reports);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error.errors);
+    }
   }
-});
-​
-​
-​
+);
+
 //GET ONE
 reportRoute.get(
   "/oneReport/:reportId",
@@ -86,14 +91,14 @@ reportRoute.get(
       const { reportId } = req.params;
       const report = await ReportModel.findById(reportId).populate("user");
       if (!report) return res.status(400).json({ msg: "Report not found!" });
-​
+
       return res.status(200).json(report);
     } catch (error) {
       return res.status(400).json(error.errors);
     }
   }
 );
-​
+
 //UPDATE
 reportRoute.put(
   "/edit/:reportId",
@@ -104,13 +109,13 @@ reportRoute.put(
     try {
       const { reportId } = req.params;
       console.log(reportId);
-​
+
       const updatedReport = await ReportModel.findByIdAndUpdate(
         { _id: reportId },
         { ...req.body },
         { new: true, runValidators: true }
       );
-​
+
       return res.status(200).json(updatedReport);
     } catch (error) {
       console.log(error);
@@ -118,10 +123,9 @@ reportRoute.put(
     }
   }
 );
-​
+
 //DELETE
 reportRoute.delete(
-
   "/delete/:reportId",
   isAuth,
   attachCurrentUser,
@@ -129,13 +133,13 @@ reportRoute.delete(
   async (req, res) => {
     try {
       const { reportId } = req.params;
-​
+
       const deletedReport = await ReportModel.findByIdAndDelete(reportId);
-​
+
       if (!deletedReport) {
         return res.status(400).json({ msg: "Report not found!" });
       }
-​
+
       const allReports = await ReportModel.find();
       const user = await UserModel.findByIdAndUpdate(
         deletedReport.avaliado,
@@ -144,9 +148,9 @@ reportRoute.delete(
         },
         { new: true, runValidators: true }
       );
-​
+
       console.log(deletedReport);
-​
+
       return res.status(200).json(allReports);
     } catch (error) {
       console.log(error);
@@ -154,5 +158,5 @@ reportRoute.delete(
     }
   }
 );
- 
+
 export default reportRoute;
